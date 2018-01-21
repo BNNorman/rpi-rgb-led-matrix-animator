767a9bc80d7e737eb33c7074a18a15f85f3f3e47
Brian <brian.n.norman@gmail.com>
2018-01-21 16:36:59 +0000

    Bug fixes
    Signed-off-by: brian <brian.n.norman@gmail.com>

--- LEDAnimator/ChainAnimBase.py
+++ LEDAnimator/ChainAnimBase.py
@@ -10,21 +10,10 @@ Sub-class of AnimBase
 from AnimBase import AnimBase
 
 class ChainAnimBase(AnimBase):
+    # nothing extra is added
+    pass
 
-    brightness=1
-    alpha=1
 
-    chain=None              # the chain for this animation
-    chainBuffer=None        # used to for rendering chains on transparent backgrounds
-
-    def __init__(self, **kwargs):
-        super(ChainAnimBase,self).__init__(**kwargs)
-
-        self.targetSpeed = self.speed
-        self.setSpeed(self.targetSpeed)
-
-    def reset(self,**kwargs):
-        super(ChainAnimBase,self).reset(**kwargs)
 
 
 
