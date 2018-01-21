77c19e62293d83428b26318e9a90cc34386b5180
Brian <brian.n.norman@gmail.com>
2018-01-21 16:44:07 +0000

    Bug fixes
    Signed-off-by: brian <brian.n.norman@gmail.com>

--- LEDAnimator/AnimSequence.py
+++ LEDAnimator/AnimSequence.py
@@ -8,20 +8,19 @@ animation in the sequence
 
 class AnimSequence():
 
-    animList=[]     # list of animations in the sequence
-    listLen=0       # and how many
-    curAnim=0       # used to getNextAnimation
+    def __init__(self,animList):
+        # animList contains a list of function pointers
+        self.animList=[]
 
-    def __init__(self,animList=[]):
         for entry in animList:
-            self.animList.append(entry)
+            self.animList.append(entry) # could use deepcopy?
 
+        self.curAnim=0
         self.listLen=len(animList)
 
         assert self.listLen<>0, "Zero length animation list. Check syntax of your animation sequence."
 
-    def getNextAnimation(self,debug=False):
+    def getNextAnimation(self):
         p=self.curAnim
         self.curAnim=(self.curAnim+1) % self.listLen #' wraps to 0
-        if debug : print "AnimSequence.getNextAnimation() returns item",p
         return self.animList[p]
