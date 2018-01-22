7b2bec7ed514ad044dd6e3ce27e5a4e009a71053
Brian <brian.n.norman@gmail.com>
2018-01-22 12:28:10 +0000

    Removed redundant code
    Signed-off-by: brian <brian.n.norman@gmail.com>

--- LEDAnimator/AnimInfo.py
+++ LEDAnimator/AnimInfo.py
@@ -14,13 +14,12 @@ class AnimInfo(object):
     animSeq = None
     fps = None
     animFunc = None
-    beginDone = False  # has begin been called?
-    #canvas = None
-    #matrix = None  # in case the animation needs a private canvas
     curPalEntry = 0
     palette = None
-    simulating = False  # default
+
+    # debugging
     debug = False
+    id="[No id]"
 
     def __init__(self,**kwargs):
         """
@@ -29,23 +28,6 @@ class AnimInfo(object):
         """
         for key,value in kwargs.iteritems():
             setattr(self,key,value)
-
-    def begin(self):
-        """
-        begin() is called once for each animation in a sequence to allow it to do any pre-run setup.
-        It selects the next animation in a sequence, cycling back to the first then calls the animation's reset() function.
-        :return: nothing, simply sets self.animFunc and calls reset()
-        """
-
-        self.animFunc=self.animSeq.getNextAnimation()
-
-        # initialise the animation for first run
-        self.animFunc.durationExpired=False
-        self.animFunc.reset()
-
-        # flag to ensure that the animation has been initialised for first run
-        self.beginDone=True
-
 
 
     def nextFrame(self,debug=False):
@@ -60,15 +42,8 @@ class AnimInfo(object):
         # initialise the next animation - setup animFunc
         #if not self.beginDone: self.begin()
 
-        #time to move to next animation in the sequence?
-        #if self.animFunc.durationExpired:
-        #    if self.debug:
-        #        print "AnimInfo.nextFrame() calling begin() for next animation in seq"
-        #    self.begin()
-
         if self.animFunc is None:
             self.animFunc = self.animSeq.getNextAnimation()
-
 
         # chain is ignored by non-chain based animations
         if self.debug: print "AnimInfo.nextFrame() Calling nextFrame() for",self.animFunc
