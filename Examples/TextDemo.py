c76d00ddeaeaa36099a11d130263f9f65563f225
Brian <brian.n.norman@gmail.com>
2018-01-22 11:51:13 +0000

    Added separate background sequence. Turned off videoCapture and DEBUG set to False
    Signed-off-by: brian <brian.n.norman@gmail.com>

--- Examples/TextDemo.py
+++ Examples/TextDemo.py
@@ -1,6 +1,6 @@
 
 '''
-TextDemo.py - cycle through DAF truck logos
+TextDemo.py - Moving text a plus fade
 
 '''
 
@@ -14,11 +14,10 @@ import PathSetter
 import sys
 import traceback
 
-from LEDAnimator import TextAnimations
+from LEDAnimator import TextAnimations,ImageAnimations
 from LEDAnimator.Animator import Animator
 import LEDAnimator.Panel as Panel
 from LEDAnimator.AnimSequence import AnimSequence
-from LEDAnimator.UtilLib import *
 from LEDAnimator.Font import *
 import LEDAnimator.Palette as Palette
 from LEDAnimator.Image import *
@@ -55,28 +54,43 @@ bdfText="Hello sweetie BDF 12pt text here!"
 bdf=Font("BDF",12)
 tLenBDF,h=bdf.getTextBbox(bdfText)
 
-cv2msg=Text(text=cv2Text,fontSize=12,fontFace=FONT_HERSHEY_SIMPLEX,
-                 fgColor=Palette.XMAS,bgColor=(125,0,125,255))
-
+#------------
 bdfmsg=Text(text=bdfText, fontSize=12, fontFace="BDF", fgColor=Palette.XMAS,
             bgColor=Palette.LOTS)
-
 
 bdfSeq= AnimSequence([
     TextAnimations.Move (duration=20, speed=0.3, fps=FPS, startPos=(64,20), endPos=(-tLenBDF,20), text=bdfmsg,
                          multiColored=True, Xpos=32,Ypos=32),
 ])
 
+#-----------
+cv2msg=Text(text=cv2Text,fontSize=12,fontFace=FONT_HERSHEY_SIMPLEX,
+                 fgColor=Palette.XMAS,bgColor=(125,0,125,255))
 cv2Seq= AnimSequence([
     TextAnimations.Move (duration=20, speed=0.3, fps=FPS, startPos=(64,40), endPos=(-tLenCV2,40),  multiColored=True,
                          text=cv2msg),
 ])
 
+#-----------
+fadeTxt=Text(text="FADE IN", fontSize=12, fontFace="BDF", fgColor=Palette.RGB,
+            bgColor=Palette.LOTS,Xpos=0,Ypos=5)
+
+fadeIn=AnimSequence([
+    TextAnimations.FadeIn(duration=10,speed=1,fps=FPS,Xpos=0,Ypos=5,text=fadeTxt,startPause=2)
+])
+
+#------------
+bkgnd=AnimSequence([
+    ImageAnimations.Wait(background=(255,0,255,255),duration=20,fps=FPS,Xpos=0,Ypos=0),
+])
+
 # create the animator object
 A= Animator(debug=DEBUG,fps=FPS)
+A.addAnimation(seq=bkgnd)
 # add the sequences the order is bottom layer to top layer
 A.addAnimation(seq=bdfSeq)  # bg images must be laid down first
 A.addAnimation(seq=cv2Seq)
+A.addAnimation(seq=fadeIn)
 
 # run the animation
 try:
