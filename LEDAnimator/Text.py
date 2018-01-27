e923187b0249a3032f3603e9b8bb250a778864c6
Brian <brian.n.norman@gmail.com>
2018-01-27 12:48:27 +0000

    Added position parameters in readiness to manage Text position handling the same way Image position handling is done.
    Signed-off-by: brian <brian.n.norman@gmail.com>

--- LEDAnimator/Text.py
+++ LEDAnimator/Text.py
@@ -29,25 +29,87 @@ class Text():
     fontFace=None       # "BDF" or FONT_HERSHEY_??
     fontSize=None       # integer, pixels
     multiColored=False  # if True each character is rendered cyclically using the Palette colours
+    Xpos,Ypos=0,0
 
     def __init__(self, **kwargs):
         for key, value in kwargs.iteritems():
             setattr(self, key, value)
 
+    def setPos(self,pos):
+        """
+        Set the current position of the text relative to the Panel.
+
+        :param tuple pos: X,Y values
+        :return None:
+        """
+        self.Xpos,self.Ypos=pos
+
+    def getPos(self):
+        """
+        Get the current position of the text relative to the Panel.
+
+        :return tuple: (x,y)
+        """
+        return self.Xpos,self.Ypos
+
     def getText(self):
+        """
+        get the text of the message.
+
+        This is a user supplied value which defaults to None
+
+        :return string: the text of the message
+        """
         return self.text
 
     def getFgColor(self):
+        """
+        gets the foreground colour of the text.
+
+        The color could be either a tuple or a Palette object
+
+        :return color or Palette:
+        """
         return self.fgColor
 
     def getBgColor(self):
+        """
+        gets the background colour of the text.
+
+        The color could be either a tuple or a Palette object
+
+        :return color or Palette:
+        """
         return self.bgColor
 
     def getFontFace(self):
+        """
+        gets the FontFace of the text.
+
+        Currently this could be 'BDF' or a cv2 FONT_HERSHEY_?? values
+
+        This is a uer parameter.
+
+        :return string or int: The font face
+        """
         return self.fontFace
 
     def getFontSize(self):
+        """
+        get the current font size
+
+        This is a user parameter
+
+        :return int: text height in pixels (hopefully)
+        """
         return self.fontSize
 
     def getMultiColored(self):
+        """
+        Return the multiColored flag.
+
+        This is a user parameter (defaults to False)
+
+        :return Bool: True or False
+        """
         return self.multiColored
