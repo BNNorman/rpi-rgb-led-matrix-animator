f5944fd696fdf36ef8396a1b195151bdb27050ca
Brian <brian.n.norman@gmail.com>
2018-02-08 22:39:49 +0000

    Removed debugging print statements.
    Signed-off-by: brian <brian.n.norman@gmail.com>

--- LEDAnimator/NumpyImage.py
+++ LEDAnimator/NumpyImage.py
@@ -489,8 +489,6 @@ class NumpyImage():
 
         if color is None: return
 
-        print "type(x)=", type(x), "type(y)=", type(y),"type(color)=",type(color)
-
         if type(color) is tuple: #(rgba)
             # oddly this code occasionally throws ValueError: cannot convert float NaN to integer
             # but x,y,color and self.out[y,x] are all integers (as observed by printing the type()
@@ -502,7 +500,6 @@ class NumpyImage():
                 return
 
         if type(color) is np.ndarray:
-            print "type(x)=",type(x),"type(y)=",type(y)
             assert type(x) is np.ndarray and type(y) is np.ndarray, "If color is a numpy ndarray then x and y must also be ndarrays."
             assert x.shape[:1] == y.shape[:1] == color.shape[:1], "Arrays must be of the same length."
 
